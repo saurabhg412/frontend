088219b11c22c2743e9e55291032a373b7c37015
saurabh gupta <saurabhgupta0122@gmail.com>
2024-03-08 20:36:32 +0530

    added security

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -1,7 +1,7 @@
 export function Quote(){
     return(
-        <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-            <div className="italic text-gray-700 mb-4">
+        <div className="bg-gray-100 p-6 rounded-lg shadow-md h-screen md:block">
+            <div className="italic text-gray-700 mb-4 flex flex-wrap justify-center items-center lg:pt-20">
                 "The customer service I received was exceptional. The support team went above and beyond to address my concerns."
             </div>
             <div className="text-lg font-semibold mb-1">
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -41,29 +41,29 @@ function Signup() {
 
     return (
         <div className="grid grid-cols-12 h-screen bg-slate-100">
-            <div className="h-screen w-full flex items-center justify-center col-span-7 flex-col rounded-lg shadow-lg border p-6">
-                <Heading></Heading>
+            <div className="h-screen w-full flex items-center justify-center col-span-12 md:col-span-7 flex-col rounded-lg shadow-lg border p-6">
+                <Heading ></Heading>
                 <div className="mb-4">
                     <label className="block text-gray-700 font-bold mb-2">Name:</label>
-                    <input name="name" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={handleChange} />
+                    <input name="name" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={handleChange} required/>
                 </div>
 
                 <div className="mb-4">
                     <label className="block text-gray-700 font-bold mb-2">Email:</label>
-                    <input name="email" type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={handleChange} />
+                    <input name="email" type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={handleChange} required/>
                 </div>
                 
 
                 <div className="mb-4">
                     <label className="block text-gray-700 font-bold mb-2">Password:</label>
-                    <input type="password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={handleChange} />
+                    <input type="password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={handleChange} required/>
                 </div>
 
                 <div className="mt-4 mb-6">
                     <button className="bg-blue-500 hover:bg-indigo-700 text-white font-bold py-2 px-10 rounded-md focus:outline-none focus:shadow-outline" onClick={submitHandler}>Signup</button>
                 </div>
             </div>
-            <div className="text-xl h-screen bg-slate-200 flex flex-col items-center justify-center font-serif p-20 col-span-5">
+            <div className="text-xl h-screen bg-slate-200 flex flex-col items-center justify-center font-serif p-20 col-span-5 ">
                 <Quote></Quote>
             </div>
         </div>
